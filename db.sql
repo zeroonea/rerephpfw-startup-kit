bf6778a0725b7f8adf62368da3db0cac89eb784d
zeroonea <zeroonea@gmail.com>
2012-10-23 18:11:02 +0700

    update fw

--- db.sql
+++ db.sql
@@ -4,13 +4,13 @@ Navicat MySQL Data Transfer
 Source Server         : localhost
 Source Server Version : 50516
 Source Host           : localhost:3306
-Source Database       : rerefolio
+Source Database       : your_db_name
 
 Target Server Type    : MYSQL
 Target Server Version : 50516
 File Encoding         : 65001
 
-Date: 2012-05-10 13:11:21
+Date: 2012-10-23 18:07:50
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -26,6 +26,7 @@ CREATE TABLE `members` (
   `first_name` varchar(255) DEFAULT NULL,
   `last_name` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
+  `password` varchar(32) DEFAULT NULL,
   `created` datetime NOT NULL,
   PRIMARY KEY (`member_id`),
   UNIQUE KEY `fbid` (`openid`)
@@ -48,8 +49,4 @@ CREATE TABLE `sessions` (
   `updated` datetime DEFAULT NULL,
   `member_id` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`session_id`,`session_key`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- ----------------------------
--- Records of sessions
--- ----------------------------
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
